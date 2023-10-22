8fbf5e804d6e340806d4a8d3c50625f48510e7b2
Deqmmbi <deqmbbi@gmail.com>
2023-10-22 12:39:06 +0300

    add side menu

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="content">
+	<h1>{$page.status}: {$page.error?.message}</h1>
+</div>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,53 +1,116 @@
-<script>
-	import Header from './Header.svelte';
-	import './styles.css';
+<script lang="ts">
+	import '../styles-reset.scss';
+	import IoIosArchive from 'svelte-icons/io/IoIosArchive.svelte';
+	import IoIosBarcode from 'svelte-icons/io/IoIosBarcode.svelte';
+	import IoIosStats from 'svelte-icons/io/IoIosStats.svelte';
+	import IoIosCheckbox from 'svelte-icons/io/IoIosCheckbox.svelte';
+	import IoIosLogOut from 'svelte-icons/io/IoIosLogOut.svelte';
+
+	function logout() {
+		console.log('logout');
+	}
 </script>
 
-<div class="app">
-	<Header />
+<div class="layout">
+	<nav class="side-menu">
+		<div>
+			<a href="/profile" class="side-menu__profile">
+				<img class="profile-icon" src="src\lib\images\default-user-logo.jpg" alt="" />
+				UserName
+			</a>
+		</div>
 
-	<main>
-		<slot />
-	</main>
+		<ul class="side-menu__items">
+			<li>
+				<a href="/" class="side-menu__item">
+					<div class="side-menu__item-icon"><IoIosCheckbox /></div>
+					Tasks
+				</a>
+			</li>
+			<li>
+				<a href="/habits" class="side-menu__item">
+					<div class="side-menu__item-icon"><IoIosBarcode /></div>
+					Habits
+				</a>
+			</li>
+			<li>
+				<a href="/statistics" class="side-menu__item">
+					<div class="side-menu__item-icon"><IoIosStats /></div>
+					Statistics
+				</a>
+			</li>
+			<li>
+				<a href="/archives" class="side-menu__item">
+					<div class="side-menu__item-icon"><IoIosArchive /></div>
+					Archives
+				</a>
+			</li>
+		</ul>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+		<button class="side-menu__logout" on:click={logout}>
+			<div class="side-menu__item-icon"><IoIosLogOut /></div>
+			Logout
+		</button>
+	</nav>
 </div>
 
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
+<slot />
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&family=Playfair:wght@400;500;700&display=swap');
+	:global(body) {
+		display: grid;
+		grid-template-columns: 300px auto;
+		background: #222831;
+		color: #f9f9f9;
+		font-family: 'Nunito', sans-serif;
+		font-size: 14px;
 	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+	.side-menu {
+		height: 100vh;
+		background-color: #2c3440;
+		font-size: 18px;
+		display: grid;
+		grid-template-rows: auto auto 1fr;
+		align-items: flex-end;
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
+		&__items {
+			padding-top: 15px;
+			display: grid;
+			row-gap: 30px;
 		}
+
+		&__item {
+			display: grid;
+			grid-template-columns: auto 1fr;
+			align-items: center;
+			padding-left: 15px;
+			column-gap: 10px;
+			&-icon {
+				width: 24px;
+				height: 24px;
+			}
+		}
+
+		&__profile {
+			display: flex;
+			align-items: center;
+			gap: 10px;
+			padding: 15px;
+		}
+
+		&__logout {
+			display: flex;
+			align-items: center;
+			gap: 10px;
+			padding: 15px;
+			cursor: pointer;
+		}
+	}
+
+	.profile-icon {
+		border-radius: 50%;
+		width: 40px;
 	}
 </style>
